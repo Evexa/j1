45bc8f775e168c6e02fe77455c875d76331cd913
Ivan <example@gmail.com>
2019-10-02 15:14:28 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,67 @@
+'use strict';
+
+
+var leftBorderWidth = 4;
+let second = 2;
+const pi = 3.14;
+
+
+console.log(second)
+
+
+// var number = 5;
+// var string = "Hello!";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let somthing;
+console.log(somthing);
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+console.log(persone.age);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+//alert("Hello World");
+
+//let answer = confirm("Are you here?")
+
+//console.log(answer);
+
+//let answer = +prompt("are you 18?", "Yes")
+
+//console.log(typeof(answer));
+
+//console.log("arr" + " - object");
+//console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked && isClose); //оператор И
+console.log(isChecked || isClose); //оператор ИЛИ
+console.log(isChecked || !isClose); //оператор ИЛИ и !NOT
+
